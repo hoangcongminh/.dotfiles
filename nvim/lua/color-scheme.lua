389a60fed3279ffaa7701c080e554b48835af2c9
hoangcongminh <congminh2320@gmail.com>
2022-04-16 23:15:15 +0700

    update colorscheme

--- nvim/lua/color-scheme.lua
+++ nvim/lua/color-scheme.lua
@@ -5,6 +5,8 @@ let g:vscode_italic_comment = 1
 
 colorscheme vscode
 
+ set cursorline
+ hi clear CursorLine
 " custom style
  highlight Normal guibg=NONE ctermbg=NONE
  highlight LineNr guibg=NONE ctermbg=NONE
@@ -12,15 +14,13 @@ colorscheme vscode
  highlight EndOfBuffer guibg=NONE ctermbg=NONE
  highlight NvimTreeNormal guibg=NONE
  highlight BufferTabpageFill guibg=none
+ highlight! CursorLine gui=underline cterm=underline guibg=none
  highlight CursorLineNr guibg=NONE 
  highlight LspDiagnosticsUnderlineInformation guifg=NONE
  highlight LspDiagnosticsUnderlineError guifg=NONE
-
- highlight NvimTreeCursorline gui=underline cterm=underline guibg=NONE
+" highlight NvimTreeCursorline gui=underline cterm=underline guibg=NONE
  highlight Comment gui=italic
  highlight link GitSignsCurrentLineBlame Comment
-
- set cursorline
- hi clear CursorLine
- hi! CursorLine gui=underline cterm=underline
+ highlight FidgetTitle ctermfg=110 guifg=#6cb6eb
+ highlight FidgetTask guibg=NONE guifg=#FFFFFF cterm=bold
 ]])
