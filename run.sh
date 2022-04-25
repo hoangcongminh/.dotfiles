fd3fb1b471652ae653dfafdc16519ee2d040ea9b
hoangcongminh <congminh2320@gmail.com>
2022-04-25 11:56:11 +0700

    config p10k.zsh

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@ ln -sf $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/.dotfiles/alacritty $HOME/.config/alacritty
 ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
