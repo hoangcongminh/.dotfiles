b00867804fb67d26cb77367f43bb01d6281782e5
hoangcongminh <congminh2320@gmail.com>
2022-05-05 12:40:08 +0700

    add karabiner config

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 ln -sf $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -sf $HOME/.dotfiles/alacritty $HOME/.config/alacritty
+ln -sf $HOME/.dotfiles/karabiner $HOME/.config/karabiner
+
 ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.dotfiles/.p10k.zsh $HOME/.p10k.zsh
