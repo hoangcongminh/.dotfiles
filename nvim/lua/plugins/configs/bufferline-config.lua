6824ca551c44a7f0cd113a1961c26dabb24e5c71
hoangcongminh <congminh2320@gmail.com>
2022-05-14 16:48:19 +0700

    update

--- nvim/lua/plugins/configs/bufferline-config.lua
+++ nvim/lua/plugins/configs/bufferline-config.lua
@@ -23,12 +23,6 @@ require("bufferline").setup {
       end
     end,
     enforce_regular_tabs = true,
-    indicator_icon = '▎',
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
     diagnostics = "nvim_lsp",
     diagnostics_update_in_insert = true,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
