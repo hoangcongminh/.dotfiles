5f675da92e9c2e74d30471b02ff0374545ea3c2f
hoangcongminh <congminh2320@gmail.com>
2022-06-20 02:32:38 +0700

    setup lsp signature

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -142,6 +142,20 @@ return require('packer').startup(
 				require 'plugins.configs.lsp-config'
 			end
 		}
+		use {
+			'ray-x/lsp_signature.nvim',
+			config = function()
+				require('lsp_signature').setup({
+					bind = true,
+					fix_pos = false,
+					auto_close_after = 15, -- close after 15 seconds
+					hint_enable = false,
+					handler_opts = {
+						border = "rounded"
+					}
+				})
+			end,
+		}
 		use { 'antoinemadec/FixCursorHold.nvim', config = function()
 			vim.g.cursorhold_updatetime = 100
 		end }
