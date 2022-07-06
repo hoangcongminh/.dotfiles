f487ccadc7acdc9f9c46340a63b737b5a5b96787
hoangcongminh <congminh2320@gmail.com>
2022-07-06 12:09:25 +0700

    update

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -310,25 +310,25 @@ return packer.startup(
 				{ 'onsails/lspkind-nvim' },
 			}
 		}
-		use { 'tzachar/cmp-tabnine',
-			run = './install.sh',
-			requires = 'hrsh7th/nvim-cmp',
-			config = function()
-				local tabnine = require('cmp_tabnine.config')
-				tabnine:setup({
-					max_lines = 1000;
-					max_num_results = 20;
-					sort = true;
-					run_on_every_keystroke = true;
-					snippet_placeholder = '..';
-					ignored_file_types = { -- default is not to ignore
-						-- uncomment to ignore in lua:
-						-- lua = true
-					};
-					show_prediction_strength = false;
-				})
-			end
-		}
+		-- use { 'tzachar/cmp-tabnine',
+		-- 	run = './install.sh',
+		-- 	requires = 'hrsh7th/nvim-cmp',
+		-- 	config = function()
+		-- 		local tabnine = require('cmp_tabnine.config')
+		-- 		tabnine:setup({
+		-- 			max_lines = 1000;
+		-- 			max_num_results = 20;
+		-- 			sort = true;
+		-- 			run_on_every_keystroke = true;
+		-- 			snippet_placeholder = '..';
+		-- 			ignored_file_types = { -- default is not to ignore
+		-- 				-- uncomment to ignore in lua:
+		-- 				-- lua = true
+		-- 			};
+		-- 			show_prediction_strength = false;
+		-- 		})
+		-- 	end
+		-- }
 		use { 'L3MON4D3/LuaSnip',
 			config = function()
 				require("luasnip.loaders.from_vscode").lazy_load()
