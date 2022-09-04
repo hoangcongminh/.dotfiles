7ca6a4fac0ef42e78813649911924e764fc19e9b
hoangcongminh <congminh2320@gmail.com>
2022-09-05 00:29:03 +0700

    setup tmux-sessionizer

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -5,4 +5,4 @@ ln -s $HOME/.dotfiles/kitty $HOME/.config/kitty
 
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
-ln -s $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
