3af0f842491bbbcca473c16e2d727817782898cd
hoangcongminh <congminh2320@gmail.com>
2022-10-05 10:44:56 +0700

    organize things

--- scripts/symlink-setup.sh
+++ scripts/symlink-setup.sh
@@ -7,5 +7,5 @@ ln -s $HOME/.dotfiles/fish $HOME/.config/fish
 ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/.dotfiles/.yabairc $HOME/.yabairc
-ln -s $HOME/.dotfiles/.skhdrc $HOME/.skhdrc
+ln -s $HOME/.dotfiles/yabai/.yabairc $HOME/.yabairc
+ln -s $HOME/.dotfiles/skhd/.skhdrc $HOME/.skhdrc
