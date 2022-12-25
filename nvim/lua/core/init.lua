48f86c8afa7b30d38603c0f2fa84427cb0f38b22
hoangcongminh <congminh2320@gmail.com>
2022-12-25 22:55:54 +0700

    feat(nvim): show cursor line only in active window

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -18,6 +18,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
 })
 
+-- show cursor line only in active window
+local cursorGrp = vim.api.nvim_create_augroup("CursorLine", { clear = true })
+vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    pattern = "*",
+    command = "set cursorline",
+    group = cursorGrp,
+})
+vim.api.nvim_create_autocmd(
+    { "InsertEnter", "WinLeave" },
+    { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
 -- Unset relativenumber in insert mode
 vim.cmd("au InsertEnter * set norelativenumber")
 vim.cmd("au InsertLeave * set relativenumber")
