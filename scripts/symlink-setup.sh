3a4016520263449df295d874adf480a374f717fa
congminh <congminh2320@gmail.com>
2023-04-09 18:37:47 +0700

    feat(zsh): zshenv

--- scripts/symlink-setup.sh
+++ scripts/symlink-setup.sh
@@ -5,6 +5,7 @@ ln -sfn $HOME/.dotfiles/kitty $HOME/.config/kitty
 ln -sfn $HOME/.dotfiles/fish $HOME/.config/fish
 
 ln -sfn $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -sfn $HOME/.dotfiles/zsh/.zshenv $HOME/.zshenv
 ln -sfn $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 ln -sfn $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
 ln -sfn $HOME/.dotfiles/yabai/.yabairc $HOME/.yabairc
