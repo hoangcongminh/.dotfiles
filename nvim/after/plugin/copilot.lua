ae8c31f4935f5c275a5153fc64d1fe77652240ac
hoangcongminh <congminh2320@gmail.com>
2023-04-26 17:06:21 +0700

    fix(nvim): fix cmp and copilot conflict

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("i", "<C-k>", "copilot#Accept()", { silent = true, script = true, expr = true })
-vim.g.copilot_no_tab_map = true
-vim.g.copilot_assume_mapped = true
+-- vim.keymap.set("i", "<C-k>", "copilot#Accept()", { silent = true, script = true, expr = true })
+-- vim.g.copilot_no_tab_map = true
+-- vim.g.copilot_assume_mapped = true
--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -32,10 +32,10 @@ cmp.setup({
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
     end,
   },
-  window = {
+  -- window = {
     -- completion = cmp.config.window.bordered(winhighlight),
     -- documentation = cmp.config.window.bordered(winhighlight),
-  },
+  -- },
   mapping = {
     ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
@@ -52,25 +52,25 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif luasnip.expand_or_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
+    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   elseif luasnip.jumpable(-1) then
+    --     luasnip.jump(-1)
+    --   else
+    --     fallback()
+    --   end
+    -- end, { "i", "s" }),
   },
   formatting = {
     format = require 'lspkind'.cmp_format({
