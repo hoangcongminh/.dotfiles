ef46efe5ef9255c74fb50b98691e1da743638c06
congminh <congminh2320@gmail.com>
2023-07-29 23:26:50 +0700

    chore(): move skhd and yabai config to config folder, update brew file

--- symlink-setup.sh
+++ symlink-setup.sh
@@ -3,15 +3,15 @@ ln -sfn $HOME/.dotfiles/config/alacritty $HOME/.config/alacritty
 ln -sfn $HOME/.dotfiles/config/karabiner $HOME/.config/karabiner
 ln -sfn $HOME/.dotfiles/config/kitty $HOME/.config/kitty
 ln -sfn $HOME/.dotfiles/config/fish $HOME/.config/fish
+ln -sfn $HOME/.dotfiles/config/yabai $HOME/.config/yabai
+ln -sfn $HOME/.dotfiles/config/skhd $HOME/.config/skhd
 
 ln -sfn $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -sfn $HOME/.dotfiles/zsh/.zshenv $HOME/.zshenv
 ln -sfn $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 
-
-ln -sfn $HOME/.dotfiles/.yabairc $HOME/.yabairc
-ln -sfn $HOME/.dotfiles/.skhdrc $HOME/.skhdrc
 ln -sfn $HOME/.dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sfn $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 ln -sfn $HOME/.dotfiles/.vimrc $HOME/.vimrc
 
+echo "Done symlink setup."
