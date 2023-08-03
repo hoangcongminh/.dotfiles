27a67f4c6d25318fb4702c23bbf63b90fcb14629
hoangcongminh <congminh2320@gmail.com>
2023-08-03 09:43:11 +0700

    fix(nvim): timer config fidget

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -7,6 +7,11 @@ return {
 			text = {
 				spinner = "meter",
 			},
+			timer = {
+				spinner_rate = 125, -- frame rate of spinner animation, in ms
+				fidget_decay = 1000, -- how long to keep around empty fidget, in ms
+				task_decay = 500, -- how long to keep around completed task, in ms
+			},
 			window = {
 				blend = 0,
 			},
