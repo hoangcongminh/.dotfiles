cbf0808b7a097a7cbc5ce0bb8e2cb94c39a7cc36
congminh <congminh2320@gmail.com>
2023-11-17 14:29:14 +0700

    chore(nvim): refactor

--- config/nvim/lua/configs/ui.lua
+++ config/nvim/lua/configs/ui.lua
@@ -1,33 +1,3 @@
-local global = require("global")
-
-require("tokyonight").setup({
-	style = "moon",
-	transparent = global.transparent,
-})
-
-require("everforest").setup({
-	transparent_background_level = 1,
-	italics = true,
-})
-
-require("rose-pine").setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = "auto",
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = "main",
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = true,
-	disable_float_background = false,
-	disable_italics = false,
-	transparent = global.transparent,
-})
-
-require("solarized-osaka").setup({
-	transparent = global.transparent,
-})
-
-vim.cmd.colorscheme(global.coloscheme)
 vim.opt.background = "dark"
 vim.o.winbar = "%m%y " .. "%{%v:lua.require'nvim-web-devicons'.get_icon_color_by_filetype(&filetype)%} " .. "%f"
 
@@ -37,3 +7,5 @@ vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 0, bg = "#333333" })
 vim.api.nvim_set_hl(0, "CursorLine", { underline = true, bg = "none", fg = "none" })
 vim.api.nvim_set_hl(0, "CursorLineNR", { bg = "none" })
+
+vim.cmd.colorscheme(require("global").coloscheme)
--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,51 @@
+local global = require("global")
+
 return {
-	-- colorscheme
-	{ "folke/tokyonight.nvim" },
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		priority = 1000,
-	},
-	{
-		"neanias/everforest-nvim",
-		version = false,
-		priority = 1000,
-	},
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    otps = {
+
+      style = "moon",
+      transparent = global.transparent,
+    },
+  },
+  {
+    "rose-pine/neovim",
+    lazy = true,
+    name = "rose-pine",
+    priority = 1000,
+    opts = {
+
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "auto",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "main",
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = true,
+      disable_float_background = false,
+      disable_italics = false,
+      transparent = global.transparent,
+    },
+  },
+  {
+    "neanias/everforest-nvim",
+    lazy = true,
+    version = false,
+    priority = 1000,
+    opts = {
+
+      transparent_background_level = 1,
+      italics = true,
+    },
+  },
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = global.transparent,
+    },
+  },
 }
