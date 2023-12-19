1f583a96f7a4ae03a48bd7e24d62c04f3b3320ec
congminh <congminh2320@gmail.com>
2023-12-19 20:17:45 +0700

    feat(nvim): lsp quick fix list key map

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -114,6 +114,7 @@ return {
             vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
             vim.keymap.set({ "n", "x" }, "<leader>=", vim.lsp.buf.format, opts)
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "<leader>a", vim.diagnostic.setqflist, opts)
             vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
             vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
             vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
