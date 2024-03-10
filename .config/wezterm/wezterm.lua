c434b14208f0eaa09033d2929439fe9ccb65cd53
congminh <congminh2320@gmail.com>
2024-03-10 13:14:13 +0700

    feat()!: install wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+local wezterm = require("wezterm")
+local colors = require("lua/rose-pine").colors()
+local window_frame = require("lua/rose-pine").window_frame()
+
+local config = {
+	font = wezterm.font_with_fallback({
+		"JetBrainsMono Nerd Font",
+		"JetBrains Mono",
+	}),
+	font_size = 12,
+	window_background_opacity = 0.8,
+	macos_window_background_blur = 10,
+	-- color_scheme = "Dracula",
+	colors = colors,
+	window_frame = window_frame,
+
+	adjust_window_size_when_changing_font_size = false,
+	enable_tab_bar = false,
+	native_macos_fullscreen_mode = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+}
+
+wezterm.on("gui-startup", function()
+	local _, _, window = wezterm.mux.spawn_window({})
+	window:gui_window():maximize()
+end)
+
+return config
