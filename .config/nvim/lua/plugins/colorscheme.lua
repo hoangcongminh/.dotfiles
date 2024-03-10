08fdb12a1657faeaa73616ecd64cfe71fab0fb71
congminh <congminh2320@gmail.com>
2024-03-10 13:15:26 +0700

    feat(nvim): change colorscheme to rose-pine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,19 @@
 return {
-  'navarasu/onedark.nvim',
-  lazy = false,
+  'rose-pine/neovim',
+  name = 'rose-pine',
   priority = 1000,
   config = function()
-    require('onedark').setup {
-      transparent = true,
+    require('rose-pine').setup {
+      styles = {
+        transparency = true,
+      },
     }
 
-    vim.cmd.colorscheme 'onedark'
+    vim.cmd.colorscheme 'rose-pine'
 
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'CursorLine', { underline = true, bg = 'none', fg = 'none' })
   end,
 }
