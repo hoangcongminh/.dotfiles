0ea2a37331287aaec9a82fcd51c600132cb7b441
congminh <congminh2320@gmail.com>
2024-09-13 16:26:11 +0700

    feat(wezterm): new wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,28 +1,36 @@
 local wezterm = require("wezterm")
 
-local config = {
-	font = wezterm.font_with_fallback({
-		"JetBrainsMono Nerd Font",
-		"JetBrains Mono",
-	}),
-	font_size = 13,
-	window_background_opacity = 0.9,
-	macos_window_background_blur = 10,
-	color_scheme = "rose-pine-moon",
-	colors = {
-		background = "black",
-	},
+local config = {}
 
-	adjust_window_size_when_changing_font_size = false,
-	enable_tab_bar = false,
-	native_macos_fullscreen_mode = false,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	"JetBrains Mono",
+})
+
+config.font_size = 14
+config.color_scheme = "Ubuntu"
+config.window_background_opacity = 0.9
+config.colors = {
+	background = "#111111",
 }
 
-wezterm.on("gui-startup", function()
-	local _, _, window = wezterm.mux.spawn_window({})
-	window:gui_window():maximize()
-end)
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.enable_scroll_bar = false
+config.window_padding = { left = 0, right = 0, top = "0cell", bottom = 0 }
+
+config.adjust_window_size_when_changing_font_size = false
+config.front_end = "OpenGL"
+config.native_macos_fullscreen_mode = false
+config.window_close_confirmation = "NeverPrompt"
 
 return config
